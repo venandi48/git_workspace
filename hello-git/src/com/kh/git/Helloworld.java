31b33d7c153b9dae1ed4b165964ec2891366288e
venandi48 <bow999@naver.com>
2022-05-09 10:36:03 +0900

    Dog객체 bark기능 호출

--- hello-git/src/com/kh/git/Helloworld.java
+++ hello-git/src/com/kh/git/Helloworld.java
@@ -1,5 +1,7 @@
 package com.kh.git;
 
+import com.kh.animal.Dog;
+
 /**
  * SCM Software Configuration Management
  *  - 코드추적(버전관리)외에 build, packaging, deploy 등 프로젝트 관련 프로세스를 관리
@@ -17,6 +19,9 @@ public class Helloworld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		Dog dog = new Dog();
+		dog.bark();
 	}
 
 }
