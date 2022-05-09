de598e564919841f1a11623b7aee3eaeb337c1c0
venandi48 <bow999@naver.com>
2022-05-09 11:12:51 +0900

    HelloJava 오류수정
    print()기능추가

--- hello-git/src/com/kh/git/HelloJava.java
+++ hello-git/src/com/kh/git/HelloJava.java
@@ -1,7 +1,7 @@
 package com.kh.git;
 
 public class HelloJava {
-	public static void main(String[] args) {
-		System.out.println("세번째 버전에 추가된 출력문~");
+	public void print() {
+		System.out.println("Hello java~");
 	}
 }
