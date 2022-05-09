7dc938b840f38622dbe05d16c72dbc88bdf4da86
venandi48 <bow999@naver.com>
2022-05-09 12:26:10 +0900

    Cat#breed 추가

--- hello-git/src/com/kh/animal/Cat.java
+++ hello-git/src/com/kh/animal/Cat.java
@@ -3,6 +3,7 @@ package com.kh.animal;
 public class Cat {
 	private String name; // master에서 작업
 	private String color; // animal에서 추가
+	private String breed; // master에서 작업
 
 	public void cry() {
 		System.out.println("야오오옹");
